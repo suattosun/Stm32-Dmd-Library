4a96d7f3602dfce4f7f646f9d43849867c317154
suattosun <44433690+suattosun@users.noreply.github.com>
2021-06-24 14:59:51 +0300

    Update dmd.h

--- src/dmd.h
+++ src/dmd.h
@@ -1,7 +1,13 @@
-
+/*
+ * dmd.h
+ *
+ *  Created on: 19 Haz 2021
+ *      Author: SUAT-ARGE
+ */
 #include "stm32f4xx_hal.h"
 #include "SystemFont5x7.h"
 #include "Arial14.h"
+
 #ifndef INC_DMD_H_
 #define INC_DMD_H_
 typedef enum {
@@ -11,11 +17,11 @@ void dmd_init( GPIO_TypeDef *Port_all ,
 		uint16_t data_pin, uint16_t clk_pin, uint16_t sclk_pin, uint16_t a_pin,
 		uint16_t b_pin, int16_t oe_pin);
 void dmd_clear();
-void dmd_config(uint8_t column , uint8_t row);
+void dmd_config(uint8_t row , uint8_t column);
 void dmd_call_data();
-void dmd_write_string(char *str, uint8_t start_column, uint8_t start_row);
-void dmd_write_char(char data, uint8_t column, uint8_t row);
-void dmd_write_char_to_byte(uint8_t data, uint8_t column, uint8_t row);
+void dmd_write_string(char *str, uint8_t start_row, uint8_t start_column);
+void dmd_write_char(char data, uint8_t row, uint8_t column);
+void dmd_write_char_to_byte(uint8_t data, uint8_t row, uint8_t column);
 void dmd_write_pixel(unsigned int bX, unsigned int bY, bool_e bPixel);
 int dmd_get_up_to_four_div(unsigned int bDiv);
 int dmd_get_up_to_four_mods(unsigned int bY);
